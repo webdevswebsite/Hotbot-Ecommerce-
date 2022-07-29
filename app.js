788464124ffa363c257e80803352d96dbc86c992
Olumayowa Martins <mayowasamuel86@gmail.com>
2022-07-29 06:58:04 +0100

    add to chart functionallity

---
+++ app.js
@@ -0,0 +1,176 @@
+const cartContainer = document.querySelector('.cart-container');
+const productList = document.querySelector('.product-list');
+const cartList = document.querySelector('.cart-list');
+const cartTotalValue = document.getElementById('cart-total-value');
+const cartCountInfo = document.getElementById('cart-count-info');
+let cartItemID = 1;
+
+eventListeners();
+
+// all event listeners
+function eventListeners(){
+    window.addEventListener('DOMContentLoaded', () => {
+        loadJSON();
+        loadCart();
+    });
+
+    // add to cart
+    productList.addEventListener('click', purchaseProduct);
+
+    // delete from cart
+    cartList.addEventListener('click', deleteProduct);
+}
+
+
+// update cart info
+function updateCartInfo(){
+    let cartInfo = findCartInfo();
+    console.log(cartInfo)
+    cartCountInfo.textContent = cartInfo.productCount;
+    cartTotalValue.textContent = cartInfo.total;
+}
+
+updateCartInfo();
+
+// load product items content form JSON file
+function loadJSON(){
+    fetch('products.json')
+    .then(response => response.json())
+    .then(data =>{
+        let html = '';
+        data.forEach(product => {
+             html += `
+          <div class = "product-item">
+                    <div class = "product-img">
+                        <img src = "${product.imgSrc}" alt = "product image">
+                        <button type = "button" class = "add-to-cart-btn">
+                            <i class = "fas fa-shopping-cart"></i>Add To Cart
+                        </button>
+                    </div>
+
+                    <div class = "product-content">
+                        <h3 class = "product-name">${product.name}</h3>
+                        <span class = "product-category">${product.category}</span>
+                        <p class = "product-price">₦${product.price}</p>
+                    </div>
+                </div>
+            `;
+        });
+        productList.innerHTML = html;
+    })
+    .catch(error => {
+        alert(`User live server or local server`);
+    })
+}
+
+// add alert to product added to cart
+// function addProductAlert(e) {
+//     onclick()
+// }
+
+// purchase product
+function purchaseProduct(e){
+    if (e.target.classList.contains('add-to-cart-btn')) {
+        let product = e.target.parentElement.parentElement;
+        getProductInfo(product);
+    }
+}
+
+function getProductInfo(product){
+    let productInfo = {
+        id: cartItemID,
+        imgSrc: product.querySelector('.product-img img').src,
+        name: product.querySelector('.product-name').textContent,
+        category: product.querySelector('.product-category').textContent,
+        price: product.querySelector('.product-price').textContent
+    }
+    cartItemID++;
+    addToCartList(productInfo);
+    saveProductInStorage(productInfo);
+    addProductAlert()
+}
+
+// add the selected product to the cart list
+
+function addToCartList(product){
+    const cartItem = document.createElement('div');
+    cartItem.classList.add('cart-item');
+    cartItem.setAttribute('data-id', `${product.id}`);
+    cartItem.innerHTML = `
+        <img src = "${product.imgSrc}" alt = "product image">
+        <div class = "cart-item-info">
+            <h3 class = "cart-item-name">${product.name}</h3>
+  
+            <span class = "cart-item-price">${product.price}</span>
+        </div>
+
+        <button type = "button" class = "cart-item-del-btn">
+            <i class = "ti-close"></i>
+        </button>
+    `;
+    cartList.appendChild(cartItem);
+}
+
+// save the product in the local storage
+function saveProductInStorage(item){
+    let products = getProductFromStorage();
+    products.push(item);
+    localStorage.setItem('products', JSON.stringify(products));
+    updateCartInfo();
+}
+
+// get all the products info if there is any in the local storage
+function getProductFromStorage(){
+    return localStorage.getItem('products') ? JSON.parse(localStorage.getItem('products')) : [];
+}
+
+// load carts product
+function loadCart(){
+    let products = getProductFromStorage();
+    if(products.length < 1){
+        cartItemID = 1; 
+    } else {
+        cartItemID = products[products.length - 1].id;
+        cartItemID++;
+    }
+    products.forEach(product => addToCartList(product));
+
+    // calculate and update UI of cart info 
+    updateCartInfo();
+}
+
+// calculate total price of the cart and other info
+function findCartInfo(){
+    let products = getProductFromStorage();
+    let total = products.reduce((acc, product) => {
+        let price = parseFloat(product.price.substr(1));
+        return acc += price;
+    }, 0); // adding all the prices
+
+    return{
+        total: total.toFixed(2),
+        productCount: products.length
+    }
+}
+
+// delete product from cart list and local storage
+function deleteProduct(e){
+    let cartItem;
+    if(e.target.tagName === "BUTTON"){
+        cartItem = e.target.parentElement;
+        cartItem.remove(); 
+    } else if(e.target.tagName === "I"){
+        cartItem = e.target.parentElement.parentElement;
+        cartItem.remove(); 
+    }
+
+    let products = getProductFromStorage();
+    let updatedProducts = products.filter(product => {
+        return product.id !== parseInt(cartItem.dataset.id);
+    });
+    localStorage.setItem('products', JSON.stringify(updatedProducts)); 
+    updateCartInfo();
+    console.log(products)
+    console.log(updatedProducts)
+
+}
